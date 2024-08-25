82f475929f8d68f8ff5b56f19fe433e69ce9ea2d
Diogo Dos Santos Lessa <diogodslessa@gmail.com>
2024-08-25 17:18:30 -0300

    bucket_con_workin

--- dags/F1_Ingestion.py
+++ dags/F1_Ingestion.py
@@ -1,19 +1,25 @@
 import datetime
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from airflow.providers.amazon.aws.operators.s3 import S3CreateObjectOperator
 
-def get_from_api_func():
-    print('test')
+bucket_name = 'data'
 
 dag = DAG(
      dag_id="F1_Ingestion",
      start_date=datetime.datetime(2021, 1, 1),
-     schedule="@daily",
+     schedule=None,
      catchup=False
 )
 
-get_from_api = PythonOperator(
-    task_id='get_from_api',
-    python_callable=get_from_api_func
+create_object = S3CreateObjectOperator(
+    task_id="create_object",
+    s3_bucket=bucket_name,
+    s3_key='test',
+    data='opt/airflow/dags/include/data/f1_project/drivers/24-08-03-19-17-49.json',
+    replace=True,
+    aws_conn_id='minio',
+    dag=dag
 )
 
+create_object
